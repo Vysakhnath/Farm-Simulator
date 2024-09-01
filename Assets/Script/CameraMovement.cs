cf47fa011ece8d8c9df70a3763bc3b38ca1c2d63
CodeBrewer <codebrewer404@gmail.com>
2024-09-01 22:28:48 +0530

    Environment change

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -9,17 +9,33 @@ public class CameraMovement : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
 
+    private Vector3 currentMousePos;
     void Start()
     {
         m_camera = GetComponent<Camera>();
+        currentMousePos = Input.mousePosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currentMousePos != Input.mousePosition)
+        {
+            //FindDirection();
+        }
         if (Input.GetKey(KeyCode.UpArrow))
         {
             m_camera.transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
         }
+    }
+
+    public void FindDirection()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Vector3  mouseWorldPos = m_camera.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, m_camera.transform.position.y));
+
+        Vector3 forward = mouseWorldPos - m_camera.transform.position;
+        m_camera.transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+        currentMousePos = Input.mousePosition;
     }
 }
